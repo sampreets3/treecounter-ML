b5cca332f58132397b1e9c83570ec7add3cee892
Sampreet <sampreets3@gmail.com>
2021-03-31 07:54:58 +0200

    Added support for Issue #4

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ cap = cv2.VideoCapture(str(args.input))
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(str(args.output), fourcc, 25.0, (800, 600))
 
-CONF_THRESH, NMS_THRESH = 0.5, 0.5
+CONF_THRESH, NMS_THRESH = 0.75, 0.5
 count = 0
 
 # Load the network
@@ -32,19 +32,26 @@ track = cv2.TrackerCSRT_create()
 layers = net.getLayerNames()
 output_layers = [layers[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
+
+
 while True:
+    # Read frame from video
     ret, frame = cap.read()
     frame = cv2.resize(frame, (800, 600))
     height, width = frame.shape[:2]
 
+    # Handle end of video exception
     if frame is None:
         break
     
+
+    # Construct blob from image
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), swapRB=True, crop=False)
     net.setInput(blob)
+
+    # Do a forward pass with the blob
     layer_outputs = net.forward(output_layers)
 
-    cv2.line(frame, (0, 450), (800, 450), (255, 50, 25), 2, cv2.LINE_AA)
     class_ids, confidences, b_boxes = [], [], []
     for output in layer_outputs:
         for detection in output:
@@ -52,10 +59,6 @@ while True:
             class_id = np.argmax(scores)
             confidence = scores[class_id]
 
-            # Initialise the tracker with bbox
-            #x,y,w,h = (detection[0:4] * np.array([width, height, width, height])).astype('int')
-            #bbox = frame[x:x+w, y:y+h]
-            #track.init(frame, bbox)
             if confidence > CONF_THRESH:
                 center_x, center_y, w, h = (detection[0:4] * np.array([width, height, width, height])).astype('int')
 
@@ -66,37 +69,36 @@ while True:
                 confidences.append(float(confidence))
                 class_ids.append(int(class_id))
 
-        #(success, box) = track.update(frame, b_boxes)
-
-        #if success:
-        #    (x, y, w, h) = [int(v) for v in box]
-        #    cv2.rectangle(frame, (x, y), (x + w, y + h),(0, 255, 0), 2)
-
     indices = cv2.dnn.NMSBoxes(b_boxes, confidences, CONF_THRESH, NMS_THRESH).flatten().tolist()
-    #print(indices)
+
+    # Draw base line
+    cv2.line(frame, (0, 500), (800, 500), (255, 50, 25), 2, cv2.LINE_AA)
 
     with open(args.labels, "r") as f:
        classes = [line.strip() for line in f.readlines()]
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
     
     for index in indices:
-    #    print(index)
        x, y, w, h = b_boxes[index]
+
        # calculate centroid
        (centroid_x, centroid_y) = utils.get_centroid(x, y, x+w, y+h)
-
        cv2.circle(frame, (centroid_x, centroid_y), 2, (125, 125, 0), 2)
-       if (centroid_y == 450) and classes[class_ids[index]] == "tree":
+
+       # Count + if centroid lies on the line
+       if (centroid_y == 500) and classes[class_ids[index]] == "tree":
            count += 1
-       cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 1)
+
+       # Display the bounding boxes    
+       cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 1)               # Bounding box
        text_label = "{}".format(classes[class_ids[index]])
        (label_width, label_height), baseline = cv2.getTextSize(text_label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
        y_label = max(y, label_height)
-       cv2.rectangle(frame, (x, y_label - label_height), (x + label_width, y_label + baseline), (0, 0, 0), cv2.FILLED)
-       cv2.putText(frame, classes[class_ids[index]], (x , y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.65, (255, 255, 255), 1)
+       cv2.rectangle(frame, (x, y_label - label_height), (x + label_width, y_label + baseline), (0, 0, 0), cv2.FILLED) # Label box
+       cv2.putText(frame, classes[class_ids[index]], (x , y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.65, (255, 255, 255), 1) # Label text
 
        text_count = "Trees: {}".format(count)
-       cv2.putText(frame, text_count, (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 128, 255), 2)
+       cv2.putText(frame, text_count, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 128, 255), 2)
     
     #Display the frame
     cv2.imshow("Frame", frame)
